4d1e129cb7502bc5d13be3a7734944e164caf1f2
Dnld <steinert.donald@gmail.com>
2015-09-24 16:48:42 -0400

    (skeleton) updates DB skeleton

--- db/userController.js
+++ db/userController.js
@@ -2,12 +2,41 @@ var User = require('./userModel.js');
 
 // creates new user record
 exports.addUser = function(data, callback) {
+  var user = {
+  email: data.email,
+  cohort: data.cohort,
+  zip: data.zip,
+  github: data.github,
+  twitter: data.twitter,
+  website: data.website,
+  gender: data.gender,
+  race: data.gender,
+  thesis: data.thesis,
+  thesisurl: data.thesisurl,
+  greenfield: data.greenfield,
+  legacy: data.legacy,
+  technologies: data.technologies,    
+  };
+  
+  // TODO -- coordinate how employment info will be passed
+  // currentemployer: [employerSchema],
+  // prioremployers: [employerSchema],
+  
+  user.save(function(err, savedUser) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    callback();
+  });
   
 };
 
+// TODO -- figure out functionality to update a user record
+
 // controller to return all user data to the client
 // exports.sendAllUsers = function(callback) {
-exports.sendUsers = function(callback) {
+exports.sendAllUsers = function(callback) {
   User.find({}, function(err, users) {
     if (err) {
       console.log(err);
--- db/userModel.js
+++ db/userModel.js
@@ -1,4 +1,12 @@
 var mongoose = require('mongoose');
+var zipcodes = require('zipcodes');
+
+var employerSchema = new mongoose.Schema({
+  name: String,
+  role: String,
+  startdate: Date,
+  enddate: Date
+});
 
 var userSchema = new mongoose.Schema({  
   email: String,
@@ -6,19 +14,34 @@ var userSchema = new mongoose.Schema({
   zip: Number,
   city: String,
   state: String,
+  latitude: Number,
+  longitude: Number,
   github: String,
   twitter: String,
   website: String,
-  age: Number,
   gender: String,
   race: String,
+  
+  // TODO -- coordinate how employment data will be passed from client
+  currentemployer: [employerSchema],
+  prioremployers: [employerSchema],
+  
+  
   thesis: String,
+  thesisurl: String,
   greenfield: String,
   legacy: String,
   technologies: Array,
 });
 
-// possible pre middleware
-  // get exact location data
+// middleware to retreive exact location data
+userSchema.pre('save', function(next) {
+  var temp = zipcodes.lookup(this.zip);
+  this.city = temp.city;
+  this.state = temp.state;
+  this.latitude = temp.latitude;
+  this.longitude = temp.longitudel;
+  next();
+});
 
 module.exports = mongoose.model('User', userSchema);
