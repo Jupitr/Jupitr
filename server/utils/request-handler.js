2b86b9f104705f202904e80af70fff4107876066
Lai 'Lain' Jiang <lain.lai.jiang@gmail.com>
2015-09-24 16:32:48 -0500

    (feat) authentication using everyauth; cleaned up server files

---
+++ server/utils/request-handler.js
@@ -0,0 +1,13 @@
+// var request = require('request');
+// var crypto = require('crypto');
+// var bcrypt = require('bcrypt-nodejs');
+// var util = require('../lib/utility');
+
+// var db = require('../app/config');
+// var Link = require('../app/models/link');
+// var User = require('../app/models/user');
+
+exports.navToLink = function(req, res) {
+};
+
+// !! may delete this file if proven to be unnecessary !!
