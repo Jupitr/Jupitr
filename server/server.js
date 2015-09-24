acb90a44544885d73ce6634df0f0c6b2d29a2a89
Lai 'Lain' Jiang <lain.lai.jiang@gmail.com>
2015-09-24 17:27:08 -0500

    (feat) auth connects to github api

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var everyauth = require('everyauth');
 var helpers = require('./utils/helpers');
 var handler = require('./utils/request-handler');
 var config = require('./utils/config');
+var https = require('https');
 
 var app = express();
 
@@ -12,12 +13,34 @@ everyauth.github
   .appId(config.github.appId)
   .appSecret(config.github.appSecret)
   .findOrCreateUser( function (session, accessToken, accessTokenExtra, githubUserMetadata) {
-    session.oauth = accessToken;
-    session.uid = githubUserMetadata.login;
-    return session.uid;
+    var user = {
+      host: 'api.github.com',
+      path: '/organiations',
+      method: 'GET'
+    };
+    helpers.sendGHRequest(https, user, function(res) {
+      var data = '';
+      var org;
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+      res.on('end', function () {
+        org = JSON.parse(data); 
+        if (helpers.checkAuth(org)) {
+          session.oauth = accessToken;
+          session.uid = githubUserMetadata.login;
+          return session.uid;       
+        }
+        else {
+          console.log('not authorized');
+        }
+      });
+    });
   })
   .redirectPath('/');
 
+// default logout path is /logout
 everyauth.everymodule.handleLogout( function (req, res) {
   req.logout(); 
   req.session.uid = null;
