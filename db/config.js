f157c0a0d375e120c77920b87aa08bea7aa5f301
Dnld <steinert.donald@gmail.com>
2015-09-28 18:24:47 -0400

    updates db config for heroku deploy

--- db/config.js
+++ db/config.js
@@ -1,16 +1,7 @@
 var mongoose = require('mongoose');
 
-// configuration for local development
-// mongoose.connect('mongodb://localhost/jupitr');
-
-// configuration for deployment
-// var uri = 'ds051833.mongolab.com:51833/heroku_r1vgthql';
-// var options = {
-//   user: 'heroku_r1vgthql',
-//   pass: '4aun3a4j2pv7oo9vvc8bup2eil'
-// }
-
-mongoose.connect('mongodb://heroku_r1vgthql:4aun3a4j2pv7oo9vvc8bup2eil@ds051833.mongolab.com:51833/heroku_r1vgthql');
+var dbUri = process.env.MONGOLAB_URI || 'mongodb://localhost/jupitr'
+mongoose.connect(dbUri);
 
 var db = mongoose.connection;
 
