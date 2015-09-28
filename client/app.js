757042423b1a8d223610eefbcd4f4650e4ae3bb8
melindab <melindab@users.noreply.github.com>
2015-09-28 13:20:26 -0500

    Add routes to app.js.

--- client/app.js
+++ client/app.js
@@ -1,10 +1,32 @@
 (function(){
-  angular
-  .module('app', [])
-  .config(function(){
-
+  angular.module('jupitr', [
+    'jupitr.services',
+    'jupitr.login',
+    // 'jupitr.home',
+    'jupitr.profile',
+    'jupitr.auth'
+  ])
+  .config(function($routeProvider){ // need to add $httpProvider?
+    $routeProvider
+      .when('/login', {
+        templateURL: 'app/login/login.html',
+        controller: 'LoginController'
+      });
+      // .when('/home', {
+      //   templateURL: '',
+      //   controller: ''
+      // });
+      .when('/profile', {
+        templateURL: 'app/profile/profile.html',
+        controller: 'profileController',
+        authenticate: true
+      });
+      .otherwise({
+        // redirectTo: '/home'
+      });
+    // Need to add $httpProvider.interceptors.push('AttachTokens');?
   })
-  .factory('', function(){
+  .factory('', function(){ // factory to attach tokens?
 
   })
   .run(function(){
