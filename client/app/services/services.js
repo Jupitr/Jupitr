c219157d7aa51334bb9ac9908ecee208019be84d
melindab <melindab@users.noreply.github.com>
2015-09-28 15:06:10 -0500

    Add additional functions to User factory.

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,14 +1,21 @@
 angular.module('jupitr.services', [])
 
 .factory('User', function($http) {
+  // if not a user, add user profile
+  // if user, update user profile
+  var getGithubData = function(user) {
 
-  var getGithubProfile = function(user) {
+  };
+
+  var getProfile = function(user) {
+
+  };
+
+  var create = function(user) {
 
   };
 
   var update = function(user) {
-    // if not a user, add user profile
-    // if user, update user profile
     return $http({
       method: 'POST',
       url: '/api/update',
