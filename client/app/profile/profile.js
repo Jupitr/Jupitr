8d99f5a24241db61ae5b53b1c9e0c8e2b1e56de4
melindab <melindab@users.noreply.github.com>
2015-09-29 17:20:07 -0500

    Fix rebase conflicts.

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -1,14 +1,18 @@
 angular.module('jupitr.profile', [])
-.controller('profileController', function($scope, User, $location) {  
-  $scope.profile = {}; 
+.controller('profileController', function($scope, User, $location) { 
+  // get profile from session. 
+  $scope.profile = User.getMyRecord(function(data) {
+    return data; // Add || {}?
+  }); 
 
   $scope.update = function(user) {
-    // deep copy of user object stored in profile object
+    // store deep copy of user object in profile object
     $scope.profile = angular.copy(user); 
-    // Update Users with updated user profile, then send to main/home page
+    // Update using User factory (see services.js) with updated user profile, 
+    // then send to profile page again
     User.update($scope.profile)
       .then(function() {
-        $location.path('/');
+        $location.path('/profile');
       })
       .catch(function(error) {
         console.log(error);
@@ -16,7 +20,7 @@ angular.module('jupitr.profile', [])
   };
 
   $scope.reset = function() {
-    // reset user object to stored profile (empty if not filled out previously)
+    // reset user object to stored profile 
     $scope.user = angular.copy($scope.profile);
   };
 
