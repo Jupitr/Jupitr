8704e3d59d6bb8e43a21fede3b3aee6acd12c897
Kevin Lee <kevinyjlee23@gmail.com>
2015-09-29 01:00:32 -0700

    Implement basic framework for home page

---
+++ client/app/home/home_animation.js
@@ -0,0 +1,8 @@
+var width = 800;
+var height = 600;
+
+var svg = d3.select("body").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+// creating US map for Geo data visual homepage
