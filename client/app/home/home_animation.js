de441901cba0d667335e86c5fb125458ccba3198
Lai 'Lain' Jiang <lain.lai.jiang@gmail.com>
2015-09-30 16:46:30 -0500

    (feat) cohort connection

--- client/app/home/home_animation.js
+++ client/app/home/home_animation.js
@@ -27,31 +27,24 @@ var dummyLocGen = [];
 var dummyLoc = [];
 // dummy data
 var dummyLength = dummy().length;
+var cohortCoords = {};
 
 dummy().forEach(function(user){
-  var x, y;
-  var coords = floorCoords([user.longitude, user.latitude], function(coords, lati, longi){
-    x = lati;
-    y = longi;
-    return coords;
-  });
+  if (user.longitude) {
+    var coordsGen = floorCoords([user.longitude, user.latitude]).slice();
 
-  if (locStoreGen[coords] !== undefined) {
-    locStoreGen[coords]++;
-    dummyLocGen.push([x, y]);
+    locStoreGen[coordsGen] = locStoreGen[coordsGen] || 0;
+    if (locStoreGen[coordsGen] === 0) {
+      dummyLocGen.push(coordsGen);
+    }
+    locStoreGen[coordsGen]++;
+    
+    var coords = [noise(user.longitude), noise(user.latitude)];
+    dummyLoc.push(coords);  
+    cohortCoords[user.cohort] = cohortCoords[user.cohort] || [];
+    cohortCoords[user.cohort].push(coords);
   }
-  else {
-    locStoreGen[coords] = 1;
-  }
-  // locStoreGen[coords] = locStoreGen[coords] === undefined ? 1 : locStoreGen[coords]++;
-
-  dummyLoc.push([noise(user.longitude), noise(user.latitude)]); 
 });
-
-// svg.append('rect')
-//   .attr('width', width)
-//   .attr('height', height)
-//   .classed('background', true);
 
 d3.json('app/home/us.json', function(err, us){
   // https://gist.github.com/markmarkoh/2969317
@@ -59,41 +52,43 @@ d3.json('app/home/us.json', function(err, us){
   if(err){
     console.log(err);
   }
+
   g.append('g')
     .attr("id", "states")
     .selectAll("path")
     .data(topojson.feature(us, us.objects.states).features)
     .enter().append("path")
     .attr("d", map);
-  
+
   g.append("path")
     .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
     .attr('id', 'state-borders')
     .attr('d', map)
     .attr('stroke', 'black')
     .attr('stroke-width', 0.1);
+
+  // path connecting people from the same cohort
+  for (var prop in cohortCoords) {
+    var color = randomColor(0.4);
+    var connectionData = cohortCoords[prop];
+    var connection = d3.svg.line()
+                           .x(function(d){return states(d)[0];})
+                           .y(function(d){return states(d)[1];})
+                           .interpolate('linear');
+    g.append('g')
+       .append('path')
+       .attr('d', connection(connectionData))
+       .attr('stroke', color)
+       .attr('stroke-width', 0.2)
+       .attr('fill', 'none');
+  }  
   
-  // precise data location
-  g.append('g')
-    .selectAll('circle')
-    .data(dummyLoc).enter()
-    .append('circle')
-    .attr('class', 'user')
-    .attr('transform', function(d) {
-      console.log(d);
-      console.log(states(d));
-      return 'translate(' + states(d) + ')'; 
-    })
-    .attr('r', 5)
-    .attr('fill', function(d) {
-      return 'rgba(255, 0, 0, 0.2)';
-    });
   
   // general data location
   g.append('g')
     .selectAll('circle')
     .data(dummyLocGen).enter()
-    .append("circle")
+    .append('circle')
     .attr('class', 'userGen')
     .attr('transform', function(d) {
       return "translate(" + states(d) + ")";  
@@ -108,15 +103,30 @@ d3.json('app/home/us.json', function(err, us){
     })
     .attr('fill', function(d) {
       var num = floorCoords(d, function(data) {
+        console.log(locStoreGen[data]);
         return locStoreGen[data];
       });
-      var r = 120;
-      var g = 120 + num * 2;
-      var b = 150 + num * 2;
-      return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + 0.05 + ')';
+      var r = 120 + num * 2;
+      var g = 120 + num * 4;
+      var b = 150 + num * 6;
+      return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + 0.30 + ')';
     })
     .attr('stroke', 'white')
-    .attr('stroke-width', 0.05);
+    .attr('stroke-width', 0.35);
+
+  // precise data location
+  g.append('g')
+    .selectAll('circle')
+    .data(dummyLoc).enter()
+    .append('circle')
+    .attr('class', 'user')
+    .attr('transform', function(d) {
+      return 'translate(' + states(d) + ')'; 
+    })
+    .attr('r', 5)
+    .attr('fill', function(d) {
+      return 'rgba(255, 0, 0, 0.25)';
+    });
 });
 
 
@@ -133,5 +143,16 @@ function floorCoords(arr, cb) {
   };
   var x = Math.floor(arr[0] * 5) / 5;
   var y = Math.floor(arr[1] * 5) / 5;
-  return cb(x + ', ' + y, x, y);
+  return cb([x, y]);
 }
+
+function randomColor(alpha) {
+  alpha = alpha || 1;
+  var r = 200;
+  var g = Math.floor(Math.random() * 150) + 100;
+  var b = Math.floor(Math.random() * 150) + 100;
+  return 'rgba(' + r + ',' + g + ',' + b + ',' + alpha + ')';
+}
+
+
+
