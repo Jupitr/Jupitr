87cfd5cf4d78439dea4046688320628b86fe7c05
Kevin Lee <kevinyjlee23@gmail.com>
2015-09-30 13:44:26 -0700

    Implement custom directive for Angular to D3 connection

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -1,4 +1,73 @@
 angular.module('jupitr.login', [])
-  .controller('loginController', function(){
+  .controller('loginController', function($scope){
+    $scope.graph = {
+      nodes: [
+        {name: 'Kevin', group: 1}, 
+        {name: 'Don', group: 2},
+        {name: 'Melinda', group: 1}, 
+        {name: 'Lain', group: 3},
+        {name: 'Kevin', group: 1}, 
+        {name: 'Don', group: 2},
+        {name: 'Melinda', group: 1}, 
+        {name: 'Lain', group: 3},
+        {name: 'Kevin', group: 1}, 
+        {name: 'Don', group: 2},
+        {name: 'Melinda', group: 1}, 
+        {name: 'Lain', group: 3},
+        {name: 'Kevin', group: 1}, 
+        {name: 'Don', group: 2},
+        {name: 'Melinda', group: 1}, 
+        {name: 'Lain', group: 3},
+        {name: 'Kevin', group: 1}, 
+        {name: 'Don', group: 2},
+        {name: 'Melinda', group: 1}, 
+        {name: 'Lain', group: 3},
+        {name: 'Kevin', group: 1}, 
+        {name: 'Don', group: 2},
+        {name: 'Melinda', group: 1}, 
+        {name: 'Lain', group: 3},
+        {name: 'Kevin', group: 1}, 
+        {name: 'Don', group: 2},
+        {name: 'Melinda', group: 1}, 
+        {name: 'Lain', group: 3}
+      ],
+      links: [
+        {'source':1,'target':0,'value':1},
+        {'source':2,'target':0,'value':8},
+        {'source':3,'target':0,'value':10},
+        {'source':3,'target':2,'value':6},
+        {"source":1,"target":0,"value":1},
+        {"source":2,"target":0,"value":8},
+        {"source":3,"target":0,"value":10},
+        {"source":3,"target":2,"value":6},
+        {"source":4,"target":0,"value":1},
+        {"source":5,"target":0,"value":1},
+        {"source":6,"target":0,"value":1},
+        {"source":7,"target":0,"value":1},
+        {"source":8,"target":0,"value":2},
+        {"source":9,"target":0,"value":1},
+        {"source":11,"target":10,"value":1},
+        {"source":11,"target":3,"value":3},
+        {"source":11,"target":2,"value":3},
+        {"source":11,"target":0,"value":5},
+        {"source":12,"target":11,"value":1},
+        {"source":13,"target":11,"value":1},
+        {"source":14,"target":11,"value":1},
+        {"source":15,"target":11,"value":1},
+        {"source":17,"target":16,"value":4},
+        {"source":18,"target":16,"value":4},
+        {"source":18,"target":17,"value":4},
+        {"source":19,"target":16,"value":4},
+        {"source":19,"target":17,"value":4},
+        {"source":19,"target":18,"value":4}
+      ]
+    }      
+  })
+  .directive('loginVis', function($window){
+    return {
+      restrict: 'EA',
+      template: '<svg width="800" height="600"></svg>',
+      link: function(scope, elem, attrs){
+      }
+    }
   });
-
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -73,4 +73,12 @@ angular.module('jupitr.services', [])
     update: update
   };
 
+})
+.factory('d3', function(){
+  var d3 = function(){
+    
+  };
+  return {
+    d3: d3
+  };
 }); 
