f40f54f9e40e08146bced26e604bb4fc164619e5
Dnld <steinert.donald@gmail.com>
2015-09-30 13:30:32 -0400

    (db) updates user inputs and seeder file

--- db/seed-data.js
+++ db/seed-data.js
@@ -8,7 +8,8 @@ var firstNames = ['Noah', 'Emma', 'Liam', 'Olivia', 'Mason', 'Sophia', 'Jacob',
 
 var lastNames = ['Smith', 'Doe', 'Miller', 'Penny', 'Tabor', 'Lee', 'Balestra',
   'Savage', 'Machen', 'Cruz', 'Potter', 'Jones', 'Peoples', 'Rourke', 'Hendricks',
-  'Gilfoyle', 'Bighetti', 'Hanneman', 'Belson', 'Hall', 'Bachman', 'Lopez'];
+  'Gilfoyle', 'Bighetti', 'Hanneman', 'Belson', 'Hall', 'Bachman', 'Lopez',
+  'Jeter', 'Martinez', 'Wang', 'Rodriguez', ];
   
 var zips = ['94121', '94110', '94014', '94132', '94133', '10014', '10128',
   '90210', '90004', '98105', '98111', '97201', '73301', '02108', '33101',
@@ -17,19 +18,24 @@ var zips = ['94121', '94110', '94014', '94132', '94133', '10014', '10128',
   '53711', '53202', '84601', '84601', '87101', '87105', '77001', '77005',
   '30301', '30305', '37115', '37203', '33601', '33605', '02113', '57702'];
 
-var genders = ['male', 'female'];
+var onsiteOrRemote = ['HR', 'HRRB'];
+
+var genders = ['male', 'female', 'other'];
 
 var races = ['Asian', 'Caucasion/White', 'Black/African-American', 'Hispanic',
-  'Native American', 'Other'];
+  'Native American', 'Middle Eastern', 'Other'];
 
 var techs = ['JavaScript', 'Node', 'MongoDB', 'Express', 'HTML', 'Angular',
-  'CSS', 'Java', 'iOS', 'Unix', 'D3', 'Backbone', 'MSQL', 'Fortran', 'vim'];
+  'CSS', 'Java', 'iOS', 'Unix', 'D3', 'Backbone', 'SQL', 'React', 'Pearl', 
+  'Ruby', 'Rails', 'Jquery', 'PHP', 'C/C++', 'Android', 'Go', 'Rust'];
   
 var companies = ['Apple', 'Uber', 'Amazon', 'Google', 'Stripe', 'Hack Reactor',
   'Intel', 'Microsoft', 'eBay', 'Walmart', 'Square', 'JP Morgan Chase',
-  'Pied Piper', 'Hooli', 'Aviato'];
+  'Pied Piper', 'Hooli', 'Aviato', 'SquareSpace', ];
 
 var roles = ['Developer', 'Engineer', 'Programmer'];
+
+var types = ['onsite', 'remote', 'freelance', 'contract'];
 
 var projects = ['Purify CSS', 'Otto', 'Mirror', 'Lamp', 'Juiptr', 'Super Duper',
   'ToDoer', 'Orange', 'Cool Project Name', 'Zig Zag', 'Stream Line', 'L7', 
@@ -44,13 +50,12 @@ var generateRandomUser = function() {
   var lastName = pickRandom(lastNames);
   var name = firstName + ' ' + lastName;
   var email = firstName + lastName + '@myemail.com';
-  var cohort = 'HRRB' + Math.floor(Math.random() * 10);
+  var cohort = pickRandom(onsiteOrRemote) + Math.floor(Math.random() * 15);
   var zip = pickRandom(zips);
-  var handle = firstName + Math.floor(Math.random() * 1000);
+  var handle = firstName.toLowerCase() + Math.floor(Math.random() * 1000);
   var githubLogin = handle;
-  var githubLink = 'http://www.github.com/' + handle;
-  var twitter = '@' + handle;
-  var website = 'http://www.' + handle.toLowerCase() + '.com'
+  var twitter = handle;
+  var website = 'http://www.' + handle+ '.com'
   var gender = pickRandom(genders);
   var race = pickRandom(races);
   var thesis = pickRandom(projects);
@@ -63,17 +68,21 @@ var generateRandomUser = function() {
   technologies.push(pickRandom(techs));
   var currentEmployer = pickRandom(companies);
   var currentEmployerRole = pickRandom(roles);
+  var currentEmployerType = pickRandom(types)
   var currentEmployerStartDate = new Date();
   var priorEmployer1 = pickRandom(companies);
   var priorEmployer1Role = pickRandom(roles);
+  var priorEmployer1Type = pickRandom(types);
   var priorEmployer1StartDate = new Date();
   var priorEmployer1EndDate = new Date();
   var priorEmployer2 = pickRandom(companies);
   var priorEmployer2Role = pickRandom(roles);
+  var priorEmployer2Type = pickRandom(types);
   var priorEmployer2StartDate = new Date();
   var priorEmployer2EndDate = new Date();
   var priorEmployer3 = pickRandom(companies);
   var priorEmployer3Role = pickRandom(roles);
+  var priorEmployer3Type = pickRandom(types);
   var priorEmployer3StartDate = new Date();
   var priorEmployer3EndDate = new Date();
 
@@ -83,7 +92,6 @@ var generateRandomUser = function() {
     email: email,
     cohort: cohort,
     zip: zip,
-    githublink: githubLink,
     twitter: twitter,
     website: website,
     gender: gender,
@@ -98,17 +106,21 @@ var generateRandomUser = function() {
     technologies: technologies ,
     currentemployer: currentEmployer,
     currentemployerrole: currentEmployerRole,
+    currentemployertype: currentEmployerType,
     currentemployerstartdate: currentEmployerStartDate,
     prioremployer1: priorEmployer1,
     prioremployer1role: priorEmployer1Role,
+    prioremployer1type: priorEmployer1Type,
     prioremployer1startdate: priorEmployer1StartDate,
     prioremployer1enddate: priorEmployer2StartDate,
     prioremployer2: priorEmployer2,
     prioremployer2role: priorEmployer2Role,
+    prioremployer2type: priorEmployer2Type,
     prioremployer2startdate: priorEmployer2StartDate,
     prioremployer2enddate: priorEmployer2EndDate,
     prioremployer3: priorEmployer3,
     prioremployer3role: priorEmployer3Role,
+    prioremployer3type: priorEmployer3Type,
     prioremployer3startdate: priorEmployer3StartDate,
     prioremployer3enddate: priorEmployer3EndDate,
   };
