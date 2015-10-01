f871f810afe3c7c87e3f11d4e550cde5c192221e
Kevin Lee <kevinyjlee23@gmail.com>
2015-09-30 23:42:33 -0700

    (feat) Add HR node and additional styling to login

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -3,63 +3,77 @@ angular.module('jupitr.login', [])
     $scope.graph = {
       nodes: [
         {name: 'Hack Reactor', group: 1},
-        {name: 'Kevin', group: 2}, 
-        {name: 'Don', group: 2},
-        {name: 'Melinda', group: 2},
-        {name: 'Lain', group: 2}, 
-        {name: 'Taylor', group: 3},
-        {name: 'Joyce', group: 3}, 
-        {name: 'Tisha', group: 3},
-        {name: 'Rod', group: 3}, 
-        {name: 'Doug', group: 4},
-        {name: 'Victoria', group: 4}, 
-        {name: 'Rex', group: 4},
-        {name: 'Verlon', group: 4}, 
-        {name: 'Corey', group: 5},
-        {name: 'Leo', group: 5}, 
-        {name: 'James', group: 5},
-        {name: 'Joe', group: 5}, 
-        {name: 'Julia', group: 6},
-        {name: 'Ben', group: 6}, 
-        {name: 'Yeon', group: 6},
-        {name: 'Erik', group: 7}, 
-        {name: 'Igor', group: 7},
-        {name: 'Sanjay', group: 7}, 
-        {name: 'David', group: 7},
-        {name: 'Max', group: 8}, 
-        {name: 'Artem', group: 8},
-        {name: 'Michael', group: 8},
-        {name: 'Boshika', group: 8}
+        {name: 'Zeus', group: 2}, 
+        {name: 'Athena', group: 2}, 
+        {name: 'Hera', group: 2}, 
+        {name: 'Hermes', group: 2}, 
+        {name: 'Artemis', group: 2}, 
+        {name: 'Apollo', group: 2}, 
+        {name: 'Poseidon', group: 2},
+        {name: 'Kevin', group: 3}, 
+        {name: 'Don', group: 3},
+        {name: 'Melinda', group: 3},
+        {name: 'Lain', group: 3}, 
+        {name: 'Taylor', group: 4},
+        {name: 'Joyce', group: 4}, 
+        {name: 'Tisha', group: 4},
+        {name: 'Rod', group: 4}, 
+        {name: 'Doug', group: 5},
+        {name: 'Victoria', group: 5}, 
+        {name: 'Rex', group: 5},
+        {name: 'Verlon', group: 5}, 
+        {name: 'Corey', group: 6},
+        {name: 'Leo', group: 6}, 
+        {name: 'James', group: 6},
+        {name: 'Joe', group: 6}, 
+        {name: 'Julia', group: 7},
+        {name: 'Ben', group: 7}, 
+        {name: 'Yeon', group: 7},
+        {name: 'Erik', group: 8}, 
+        {name: 'Igor', group: 8},
+        {name: 'Sanjay', group: 8}, 
+        {name: 'David', group: 8},
+        {name: 'Max', group: 9}, 
+        {name: 'Artem', group: 9},
+        {name: 'Michael', group: 9},
+        {name: 'Boshika', group: 9}
       ],
       links: [
-        {'source':1,'target':0,'value':1},
-        {'source':2,'target':0,'value':8},
-        {'source':3,'target':0,'value':10},
-        {'source':3,'target':2,'value':6},
-        {"source":1,"target":0,"value":1},
-        {"source":2,"target":0,"value":8},
-        {"source":3,"target":0,"value":10},
-        {"source":3,"target":2,"value":6},
-        {"source":4,"target":0,"value":1},
-        {"source":5,"target":0,"value":1},
-        {"source":6,"target":0,"value":1},
-        {"source":7,"target":0,"value":1},
-        {"source":8,"target":0,"value":2},
-        {"source":9,"target":0,"value":1},
-        {"source":11,"target":10,"value":1},
-        {"source":11,"target":3,"value":3},
-        {"source":11,"target":2,"value":3},
-        {"source":11,"target":0,"value":5},
-        {"source":12,"target":11,"value":1},
-        {"source":13,"target":11,"value":1},
-        {"source":14,"target":11,"value":1},
-        {"source":15,"target":11,"value":1},
-        {"source":17,"target":16,"value":4},
-        {"source":18,"target":16,"value":4},
-        {"source":18,"target":17,"value":4},
-        {"source":19,"target":16,"value":4},
-        {"source":19,"target":17,"value":4},
-        {"source":19,"target":18,"value":4}
+        {'source':0,'target':0,'value':0},
+        {'source':0,'target':1,'value':1},
+        {'source':0,'target':2,'value':3},
+        {'source':0,'target':3,'value':5},
+        {"source":0,"target":4,"value":7},
+        {"source":0,"target":5,"value":9},
+        {"source":0,"target":6,"value":11},
+        {"source":0,"target":7,"value":13},
+        {"source":1,"target":8,"value":1},
+        {"source":1,"target":9,"value":1},
+        {"source":1,"target":10,"value":1},
+        {"source":1,"target":11,"value":1},
+        {"source":2,"target":12,"value":2},
+        {"source":2,"target":13,"value":1},
+        {"source":2,"target":14,"value":1},
+        {"source":2,"target":15,"value":3},
+        {"source":3,"target":16,"value":3},
+        {"source":3,"target":17,"value":5},
+        {"source":3,"target":18,"value":1},
+        {"source":3,"target":19,"value":1},
+        {"source":4,"target":20,"value":1},
+        {"source":4,"target":21,"value":1},
+        {"source":4,"target":22,"value":4},
+        {"source":4,"target":23,"value":4},
+        {"source":5,"target":24,"value":4},
+        {"source":5,"target":25,"value":4},
+        {"source":5,"target":26,"value":4},
+        {"source":6,"target":27,"value":4},
+        {"source":6,"target":28,"value":4},
+        {"source":6,"target":29,"value":4},
+        {"source":6,"target":30,"value":4},
+        {"source":7,"target":31,"value":4},
+        {"source":7,"target":32,"value":4},
+        {"source":7,"target":33,"value":4},
+        {"source":7,"target":34,"value":4}
       ]
     }      
   })
@@ -67,15 +81,18 @@ angular.module('jupitr.login', [])
     return {
       restrict: 'EA',
       link: function(scope, elem, attrs){
+
+        var color = d3.scale.category20();
+
         var svg = d3.select(elem[0])
           .append('svg')
           .attr('width', '800')
           .attr('height', '600');
         
         var forceGraph = d3.layout.force()
-          .size([800, 600])
+          .size([800, 700])
           .distance(100)
-          .charge(-100)
+          .charge(-400)
           .gravity(0.05);
 
         var drawGraph = function(){
@@ -90,8 +107,11 @@ angular.module('jupitr.login', [])
             .enter().append("line")
             .attr("class", "link");
 
+          var studentNodes = scope.graph.nodes.slice(1);
+          var hr = scope.graph.nodes.slice(0,1)
+
           var node = svg.selectAll(".node")
-            .data(scope.graph.nodes)
+            .data(studentNodes)
             .enter().append("g")
             .attr("class", "node")
             .call(forceGraph.drag);
@@ -103,9 +123,32 @@ angular.module('jupitr.login', [])
             .attr("width", 25)
             .attr("height", 25);
 
+          var hrNode = svg.selectAll(".hr")
+            .data(hr)
+            .enter().append("g")
+            .attr("class", "hr")
+            .call(forceGraph.drag);
+
+          hrNode.fixed = true;
+
+          hrNode.append("image")
+            .attr("xlink:href", "http://www.hackreactor.com/favicon.ico")
+            .attr("x", -8)
+            .attr("y", -8)
+            .attr("width", 25)
+            .attr("height", 25);
+
           node.append("text")
             .attr("dx", 20)
             .attr("dy", ".35em")
+            .text(function(d) { return d.name })
+            .style('fill', function(d){return color(d.group);})
+            .style('font-family', 'Courier');
+
+          hrNode.append("text")
+            .attr("dx", 20)
+            .attr("dy", ".35em")
+            .style('font-family', 'Courier')
             .text(function(d) { return d.name });
 
           forceGraph.on("tick", function() {
@@ -115,6 +158,7 @@ angular.module('jupitr.login', [])
               .attr("y2", function(d) { return d.target.y; });
 
             node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+            hrNode.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
           });
         };
 
