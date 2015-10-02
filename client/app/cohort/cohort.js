7f64faa0de97da23752be5a119f249e8c0304775
Kevin Lee <kevinyjlee23@gmail.com>
2015-10-01 17:27:29 -0700

    (feat) Add team json file and custom directive for cohort

--- client/app/cohort/cohort.js
+++ client/app/cohort/cohort.js
@@ -1,2 +1,16 @@
 angular.module('jupitr.cohort', [])
-  .controller('cohortController', function($scope){});
+  .controller('cohortController', function($scope){
+
+  })
+  .directive('cohortVis', function(){
+    return {
+      restrict: 'EA',
+      link: function(scope, elem, attrs){
+        var svg = d3.select(elem[0])
+          .append('svg')
+          .attr('width', '800')
+          .attr('height', '600');
+          // .on('mouseleave', zoomout)
+      }
+    };
+  });
