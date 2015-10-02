904e4060c96912dae328307de600d9715caad6ae
Kevin Lee <kevinyjlee23@gmail.com>
2015-10-01 23:57:27 -0700

    (feat) Implement cohort page functionality

--- client/app/cohort/cohort.js
+++ client/app/cohort/cohort.js
@@ -6,7 +6,7 @@ angular.module('jupitr.cohort', [])
     return {
       restrict: 'EA',
       link: function(scope, elem, attrs){
-        var width = window.innerWidth;
+        var width = window.innerWidth*0.90;
         var height = window.innerHeight - 300;
 
         var svg = d3.select(elem[0])
@@ -27,26 +27,25 @@ angular.module('jupitr.cohort', [])
             .attr('id', function(d, i){return 'mainName' + i;});
 
           var students = root.bubbles.length;
-          var color = d3.scale.category20();
           var xCord = width/(1+3*students);
           var yCord = Math.ceil(width/students*2);
 
           studentName.append('circle')
             .attr({
               'class': 'parent',
-              'r': '85',
-              'cx': function(d, i){return (width/4.5) * (i) + 185;},
-              'cy': '345'
+              'r': '90',
+              'cx': function(d, i) {return xCord*(2.75*(1+i)-1);},
+              'cy': (yCord + xCord)/3.3
             })
-            .style('fill', '#254255')
-            .style('opacity', 0.2)
+            .style('fill', '#888')
+            .style('opacity', '0.5')
             .on('mouseover', function(d, i){return zoom(d, i);});
 
           studentName.append('text')
             .attr({
               'class': 'parentText',
-              'x': function(d, i){return (width/4.5) * (i) + 185;},
-              'y': '355',
+              'x': function(d, i) {return xCord*(2.75*(1+i)-1);},
+              'y': (yCord + xCord)/3.3,
               'font-size': 25,
               'text-anchor': 'middle'
             })
@@ -55,23 +54,23 @@ angular.module('jupitr.cohort', [])
             .on('mouseover', function(d, i){return zoom(d, i);});
 
           for(var j = 0; j < students; j++){
-            var descriptBubbles = svg.selectAll('.descriptors')
+            var descriptBubbles = svg.selectAll('.child' + j)
               .data(root.bubbles[j].description)
               .enter()
               .append('g');
 
               descriptBubbles.append('circle')
                 .attr({
-                  'class': 'child',
-                  'r': '40',
-                  'cx': function(d,i) {return (xCord*(3*(j+1)-1) + xCord*1.5*Math.cos((i-1)*45/180*3.1415926) -160);},
-                  'cy': function(d,i) {return ((yCord+xCord)/3 + xCord*1.5*Math.sin((i-1)*45/180*3.1415926));},
+                  'class': 'child' + j,
+                  'r': '45',
+                  'cx': function(d,i) {return (xCord*(2.75*(j+1)-1) + xCord*1.5*Math.cos((i-1)*45/180*3.1415926));},
+                  'cy': function(d,i) {return ((yCord+xCord)/3.3 + xCord*1.5*Math.sin((i-1)*45/180*3.1415926));},
                   'cursor': 'pointer',
                   'font-size': '6'
                 })
                 .style({
-                  'opacity': '0.2',
-                  'fill': function(d, i){return color(i);}
+                  'opacity': '0.5',
+                  'fill': '#888'
                 })
                 .text(function(d){return d.name;})
                 .on('click', function(d, i){
@@ -80,12 +79,14 @@ angular.module('jupitr.cohort', [])
 
               descriptBubbles.append('text')
                 .attr({
-                  'class': 'childText',
-                  'x': function(d,i) {return (xCord*(3*(j+1)-1) + xCord*1.5*Math.cos((i-1)*45/180*3.1415926)-160);},
-                  'y': function(d,i) {return ((yCord+xCord)/3 + xCord*1.5*Math.sin((i-1)*45/180*3.1415926));},
-                  'font-size': 10,
+                  'class': 'childText' + j,
+                  'x': function(d,i) {return (xCord*(2.75*(j+1)-1) + xCord*1.5*Math.cos((i-1)*45/180*3.1415926));},
+                  'y': function(d,i) {return ((yCord+xCord)/3.3 + xCord*1.5*Math.sin((i-1)*45/180*3.1415926));},
+                  'font-size': '10',
+                  'font-weight': 'bold',
                   'cursor': 'pointer',
-                  'text-anchor': 'middle'
+                  'text-anchor': 'middle',
+                  'fill': 'blue'
                 })
                 .text(function(d){return d.name;});
           }
@@ -102,23 +103,65 @@ angular.module('jupitr.cohort', [])
               .attr('cx', function(d, ii){
                 if(i === ii){
                   return xCord*(3*(1+ii)-1) - 0.6*xCord*(ii-1);
+                }else if(ii < i){
+                  return xCord*0.6*(3*(1+ii)-1);
                 }else{
-                  if(ii < i){
-                    return xCord*0.6*(3*(1+ii)-1);
-                  }else{
-                    return xCord*(students*3+1) - xCord*0.6*(3*(students-ii)-1);
-                  }
+                  return xCord*(students*3+1) - xCord*0.6*(3*(students-ii)-1);
                 }
               })
               .attr('r', function(d, ii){
                 if(i === ii){
-                  return xCord * 1.8;
+                  return xCord * 1.2;
                 }else{
-                  return xCord * 0.8;
+                  return xCord * 0.6;
                 }
               });
 
-            
+            transition.selectAll('.parentText')
+              .attr('x', function(d ,ii){
+                if(i === ii) {
+                  return xCord*(3*(1+ii)-1) - 0.6*xCord*(ii-1);
+                }else if(ii < i){
+                  return xCord*0.6*(3*(1+ii)-1);
+                }else{
+                  return xCord*(students*3+1) - xCord*0.6*(3*(students-ii)-1);
+                }
+              })
+              .attr("font-size", function(d,ii){
+                if(i === ii){
+                  return 30*1.5;
+                }
+                else{
+                  return 30*0.6;    
+                }          
+              });
+
+            var signSide = -1;
+            for(var k = 0; k < students; k++) {
+              signSide = 1;
+              if(k < students/2){
+                signSide = 1;
+              } 
+              transition.selectAll(".childText" + k)
+                .attr("x", function(d,i) {return (xCord*(3*(k+1)-1) - 0.6*xCord*(k-1) + signSide*xCord*2.5*Math.cos((i-1)*45/180*3.1415926));})
+                .attr("y", function(d,i) {return ((yCord+xCord)/3.2 + signSide*xCord*2.5*Math.sin((i-1)*45/180*3.1415926));})
+                .attr("font-size", function(){
+                  return (k==i)?12:6;
+                })
+                .style("opacity",function(){
+                  return (k==i)?1:0;
+                });
+                     
+              transition.selectAll(".child" + k)
+                .attr("cx", function(d,i) {return (xCord*(3*(k+1)-1) - 0.6*xCord*(k-1) + signSide*xCord*2.5*Math.cos((i-1)*45/180*3.1415926));})
+                .attr("cy", function(d,i) {return ((yCord+xCord)/3.2 + signSide*xCord*2.5*Math.sin((i-1)*45/180*3.1415926));})
+                .attr("r", function(){
+                  return (k==i)?(xCord*0.40):(xCord/3.0);               
+                })
+                .style("opacity", function(){
+                  return (k==i)?0.2:0;                  
+                }); 
+            }    
           }
         });
       }
