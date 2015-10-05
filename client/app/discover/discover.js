41d4e962c9dcc9ab595732a79ff1af5ce00a980b
Dnld <steinert.donald@gmail.com>
2015-10-05 14:16:37 -0400

    (fix) adds comments to discover.js

--- client/app/discover/discover.js
+++ client/app/discover/discover.js
@@ -1,5 +1,8 @@
 angular.module('jupitr.discover', [])
 
 .controller('discoverController', function($scope, User) {
+  
+  // retrieves all user profiles from local storage to display on discover page
   $scope.profiles = JSON.parse(window.localStorage.getItem('hrr8.jupitr'));
+  
 });
